894dce0cab907c9396be0881bbde016bff886031
ReneFloresG <bineprepa@hotmail.com>
2015-09-09 11:09:37 -0500

    Ej1-1.scala

---
+++ Ej1-1.scala
@@ -0,0 +1,16 @@
+println ("Primera claificacion")
+  val clalificacion1=readFloat()
+println ("Segunda claificacion")
+  val clalificacion2=readFloat()
+println ("Tercera claificacion")
+  val clalificacion3=readFloat()
+println ("Examen Final")
+  val clalificacionExamenFinal=readFloat()
+println ("Trabajo Final")
+  val Trabajo Final=readFloat()
+
+val promedio=(calificacion1+calificacion2+calificacion3)/3
+
+val final=promedio*.55+calificacionExamenFinal*.3+TrabajoFinal*.15
+
+println ("Calificacion"+Final)
