14be421ba9676b3c57f651ac21b26feeef250d0f
ReneFloresG <bineprepa@hotmail.com>
2015-09-11 08:10:46 -0500

    Ej 2-2.scala

---
+++ Ej2-2.scala
@@ -0,0 +1,14 @@
+println ("Precio de piezas")
+  val Num de piezas=readFloat()
+
+if (Precio de piezas % 3 == 0) {
+  println(Precio de piezas<500)
+    val Inversion propia1=(Precio de piezas/100)*55
+    val Prestamo banco1=(Precio de piezas/100)*30
+    val Credito fabricante=Precio de piezas-(Inversion propia+Prestamo bancario)
+    val Credito total=Credito fabricante+((Credito fabricante/100)*20)
+} else {
+  println(Precio de piezas>500)
+    val Inversion propia2=(Precio de piezas/100)*70
+    val Credito fabricante2=Precio de piezas-Inversion propia
+    val Credito Total=Credito fabricante+((Credito fabricante/100)*20)
