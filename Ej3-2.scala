3cd015779acfeaa1dd33f0135c2d05f0d44c59e1
ReneFloresG <bineprepa@hotmail.com>
2015-10-01 18:14:17 -0500

    Create Ej3-2.scala

---
+++ Ej3-2.scala
@@ -0,0 +1,14 @@
+println("Edad")
+  val Edad=readFloat()
+println("Antiguedad empleo")
+  val Antiguedad empleo=readFloat()
+
+
+
+if (Edad, Antiguedad empleo % 2 == 0) {
+  println(Edad>=60 , Antiguedad empleo<25 + "Por Edad")
+} else {
+  println(Edad<60, Antiguedad empleo>=25 + "Por Antiguedad joven")
+} else {
+  println(Edad>=60, Antiguedad empleo>=25 + "Por Antiguedad Adulta")
+}
