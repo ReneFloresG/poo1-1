7eca15aeb862d9e0b1152e5847aa7295cf5c3695
ReneFloresG <bineprepa@hotmail.com>
2015-10-01 18:17:38 -0500

    Create Ej4-2.scala

---
+++ Ej4-2.scala
@@ -0,0 +1,33 @@
+print("Elefantes, Opcion 1")
+print("Jirafas, Opcion 2")
+print("Chimpances, Opcion 3")
+
+opcion match {
+case 1 => {
+
+for (i <- 1 to 20) {
+println(i)
+
+var Edad de los Elefantes = new Array[String](40)
+
+println ("Edad de los Elefantes")
+val Edad de los Elefantes=readFloat()
+
+}
+
+
+
+opcion match {
+case 2 => {
+
+for (i <- 1 to 15) {
+println(i)
+
+var Edad de los Jirafas = new Array[String](40)
+
+println ("Edad de los Jirafas")
+val Edad de los Jirafas=readFloat()
+
+}
+
+
