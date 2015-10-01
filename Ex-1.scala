a9203c2e0a9e504c701d0e8a945302fb98fca106
ReneFloresG <bineprepa@hotmail.com>
2015-10-01 18:19:40 -0500

    Create Ex-1.scala

---
+++ Ex-1.scala
@@ -0,0 +1,26 @@
+var numero = 0.0
+
+var cantidadPositivo = 0
+
+var cantidadNegativo = 0
+
+var cantidadNeutro = 0
+
+for (i <- 1 to 10) {
+println("Ingresa tu numero")
+numero = readFloat()
+
+if (number > 0) 
+cantidadPositivo + = 1
+} else if {
+cantidadNegativo + = 1
+} else  {
+cantidadNeutros + = 1
+}
+}
+
+
+println("Ingresaste"+ cantidadPositivos + "Numeros positivos")
+println("Ingresaste"+ cantidadNegativos + "Numeros negativos")
+println("Ingresaste"+ cantidadNeutros + "Numeros neutros")
+
