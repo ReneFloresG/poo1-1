5679cdfc2db264eaf1d7795a0b8631ef861bcdc3
ReneFloresG <bineprepa@hotmail.com>
2015-10-01 18:22:04 -0500

    Create Ex-4.scala

---
+++ Ex-4.scala
@@ -0,0 +1,18 @@
+var kilos = 0.0
+var totalCliente = 0.0
+var totalTienda = 0.0
+
+println("¿cuanto cuesta el kilo de naranja?")
+Val precioKilo = readFloat()
+
+for (i <- to 15) {
+println("¿cuantos kilos de naranja compraste?")
+Kilos = readFloat()
+totalCliente = precioKilos * Kilos
+if (kilos > 10) {
+totalCliente = totalCliente * 0.85
+} 
+totalTienda += totalCliente
+println("Vas a pagar: " + totalCliente)
+}
+println("La tienda va a recibir" + totalTienda)
