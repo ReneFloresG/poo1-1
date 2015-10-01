94a7330841a2c21f33df698cfb9da71b6f82e50e
ReneFloresG <bineprepa@hotmail.com>
2015-10-01 18:15:33 -0500

    Create Ej4-1.scala

---
+++ Ej4-1.scala
@@ -0,0 +1,32 @@
+for (i <- 1 to 10) {
+println(i)
+
+var Tiempo de prueba = new Array[String](10)
+
+println ("Tiempo de prueba")
+val Tiempo de prueba=readFloat()
+
+if (number % 2 == 0) {
+println(Tiempo de prueba>16 + "No Apto" Case 2)
+} else {
+println(Tiempo de prueba<16 + "No Apto" Case 2)
+}
+
+}
+
+
+
+var Tiempo de prueba = new Array[String](10)
+
+Promedio de tiempos = (Tiempos de prueba1+Tiempos de prueba3+Tiempos de prueba4+Tiempos de prueba5+v+Tiempos de prueba6+Tiempos de prueba7+Tiempos de prueba8+v+Tiempos de prueba9+Tiempos de prueba10)/10
+
+if (number % 2 == 0) {
+println(Promedio de tiempos>15 + "Fallo")
+} else {
+println(Promedio de tiempos<15 + "Es Apto")
+}
+
+option match {
+case 1 => Condicion1()
+case 2 => Condicion2()
+}
