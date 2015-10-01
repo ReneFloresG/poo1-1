8d984c70fb58458516d56c6e0fe9e770a1e14114
ReneFloresG <bineprepa@hotmail.com>
2015-10-01 18:20:36 -0500

    Create Ex-2.scala

---
+++ Ex-2.scala
@@ -0,0 +1,17 @@
+var menor = 11.0
+
+var calificaciones = 0.0
+
+for (i <- 1 to 40) {
+println("Ingresa tu calificacion")
+calificacion = readFloat()
+
+if (calificacion < menor) {
+menor = calificacion
+} else {
+println(number + " is odd")
+}
+
+}
+
+println ("La calificacion es" + menor)
