cddc55e32e4178ba6bcf7beebf40500cd1bf0a2d
ReneFloresG <bineprepa@hotmail.com>
2015-10-07 19:47:42 -0500

    Update Ej1-3.scala

--- Ej1-3.scala
+++ Ej1-3.scala
@@ -1,8 +1,15 @@
-println("cantidad1")
-  val Cantidad1=readFloat()
-println("cantidad2")
-  val Cantidad2=readFloat()
-println("cantidad3")
-  val Cantidad3=readFloat()
+println("Inversion 1")
+  var Inversion1 = readFloat()
+println("Inversion 2")
+  var Inversion2 = readFloat()
+println("Inversion 3")
+  var Inversion3 = readFloat()
 
-valtotal=
+val total = Inversion1+Inversion2+Inversion3
+
+  println("Procentaje 1")
+  var Procentaje1 = val total/Inversion1
+  println("Procentaje 2")
+  var Procentaje2 = val total/Inversion2
+  println("Procentaje 3")
+  var Procentaje3 = val total/Inversion3
