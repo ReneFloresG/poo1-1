3047ef761f2d402fed3675f7e61847cd790b52f9
ReneFloresG <bineprepa@hotmail.com>
2015-10-07 20:14:51 -0500

    Update Ej3-2.scala

--- Ej3-2.scala
+++ Ej3-2.scala
@@ -1,14 +1,34 @@
+var continuar = 'S'
+var i = 1
+do {
+
 println("Edad")
-  val Edad=readFloat()
+  var Edad = readFloat()
+  
 println("Antiguedad empleo")
-  val Antiguedad empleo=readFloat()
+  var Antiguedad empleo = readFloat()
 
-
-
-if (Edad, Antiguedad empleo % 2 == 0) {
-  println(Edad>=60 , Antiguedad empleo<25 + "Por Edad")
-} else {
-  println(Edad<60, Antiguedad empleo>=25 + "Por Antiguedad joven")
-} else {
-  println(Edad>=60, Antiguedad empleo>=25 + "Por Antiguedad Adulta")
+if (Edad>60 , Antiguedad empleo<25 + "Por Edad"){
 }
+if (Edad=60 , Antiguedad empleo<25 + "Por Edad"){
+}
+if (Edad<60 , Antiguedad empleo>25 + "Por Antiguedad joven") {
+}
+if (Edad<60 , Antiguedad empleo=25 + "Por Antiguedad joven") {
+} 
+if (Edad>60 , Antiguedad empleo=25 + "Por Antiguedad Adulta") {
+}
+if (Edad=60 , Antiguedad empleo=25 + "Por Antiguedad Adulta") {
+}
+if (Edad=60 , Antiguedad empleo>25 + "Por Antiguedad Adulta") {
+}
+if (Edad>60 , Antiguedad empleo>25 + "Por Antiguedad Adulta") {
+}
+
+var Edad = new Array [Float] (100 000 000 000)
+
+println ("Deseas continuar (S/N)")
+continuar = readChar()
+i += 1
+
+} while (continuar == 'S')
