eddd6eb96308c3618f704eb4ad56883a02554953
ReneFloresG <bineprepa@hotmail.com>
2015-10-07 20:23:32 -0500

    Update Ej4-1.scala

--- Ej4-1.scala
+++ Ej4-1.scala
@@ -1,29 +1,24 @@
+Condicion1()
 for (i <- 1 to 10) {
 println(i)
 
-var Tiempo de prueba = new Array[String](10)
-
 println ("Tiempo de prueba")
-val Tiempo de prueba=readFloat()
-
-if (number % 2 == 0) {
-println(Tiempo de prueba>16 + "No Apto" Case 2)
-} else {
-println(Tiempo de prueba<16 + "No Apto" Case 2)
-}
-
-}
-
-
+var Tiempo de prueba = readFloat()
 
 var Tiempo de prueba = new Array[String](10)
+}
 
-Promedio de tiempos = (Tiempos de prueba1+Tiempos de prueba3+Tiempos de prueba4+Tiempos de prueba5+v+Tiempos de prueba6+Tiempos de prueba7+Tiempos de prueba8+v+Tiempos de prueba9+Tiempos de prueba10)/10
+if (Tiempo de prueba>16 + "No Apto") {
+  case 2 } 
+if (Tiempo de prueba<16 + "Apto") {
+}
 
-if (number % 2 == 0) {
-println(Promedio de tiempos>15 + "Fallo")
-} else {
-println(Promedio de tiempos<15 + "Es Apto")
+Condicion2()
+Promedio de tiempos = (var Tiempo de prueba)/10
+
+if (Promedio de tiempos>15 + "Fallo") {
+} 
+if (Promedio de tiempos<15 + "Es Apto") {
 }
 
 option match {
