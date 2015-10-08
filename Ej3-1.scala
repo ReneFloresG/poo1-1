98ae5ff6ec779d2ffbfa48baa5322552d74f6648
ReneFloresG <bineprepa@hotmail.com>
2015-10-07 20:02:23 -0500

    Update Ej3-1.scala

--- Ej3-1.scala
+++ Ej3-1.scala
@@ -1,23 +1,21 @@
 println ("Valor compra")
-val Valor compra=readFloat()
+val ValorCompra = readFloat()
 
 println ("Pelota")
-val Pelota=readString()
+val Pelota = readString()
 
-
-if (Pelota % 2 == 0) {
-  println(Blanca + "Sin descuento")
-    val Blanca=Valor compra-0
-} else {
-  println(Verde + "10 por ciento de descuento")
-    val Verde=Valor compra-((Valor compra/100)*10)
-} else {
-  println(Amarillo + "25 por ciento de descuento")
-    val Amarillo=Valor compra-((Valor compra/100)*25)
-} else {
-  println(Azul + "50 por ciento de descuento")
-    val Azul=Valor compra-((Valor compra/100)*50)
-} else {
-  println(Roja + "100 por ciento de descuento")
-    val Roja=Valor compra-Valor compra
+if (Blanca + "Sin descuento") {
+    val Blanca = Valor compra-0
+} 
+if (Verde + "10 por ciento de descuento") {
+    val Verde = Valor compra-((Valor compra/100)*10)
+} 
+if (Amarillo + "25 por ciento de descuento") {
+      val Amarillo = Valor compra-((Valor compra/100)*25)
+} 
+if (Azul + "50 por ciento de descuento") {
+    val Azul = Valor compra-((Valor compra/100)*50)
+} 
+if (Roja + "100 por ciento de descuento") {
+    val Roja = Valor compra-Valor compra
 }
