cb711b6083b264e3ab7b41a6e7954a408e19b28c
ReneFloresG <bineprepa@hotmail.com>
2015-11-23 11:24:18 -0600

    Create Ex-Arboles.scala

---
+++ Ex-Arboles.scala
@@ -0,0 +1,14 @@
+println ("Superficie de la hectarea")
+val terreno = readFloat()
+
+if (terreno > 1000000){
+val pino = (terreno/100) * 70
+val cedro = (terreno/100) * 10
+val oyamel = (terreno/100) * 20
+}
+
+if (terreno <= 1000000){
+val pino = (terreno/100) * 50
+val cedro = (terreno/100) * 20
+val oyamel = (terreno/100) * 30
+}
