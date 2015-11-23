26e46d4616d8907ba3d7cb464ab67688511a1d85
ReneFloresG <bineprepa@hotmail.com>
2015-11-23 11:04:06 -0600

    Create Calificaciones.scala

---
+++ Calificaciones.scala
@@ -0,0 +1,12 @@
+val calif = 0.0
+val calificacionMenor = 11
+
+for (x <- 1 to 40) {
+println("Pon tu calificacion")
+nota = readFloat()
+
+if (nota < calificacionMenor){
+calificacion menor = nota } 
+}
+
+println ("La calificación menor es:" + calificacionMenor ) 
